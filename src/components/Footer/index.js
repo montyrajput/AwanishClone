354f9796fdce443e14fc9dd933aaf40b2b2cdbfb
montyrajput <montysinghrajput590@gmail.com>
2025-01-11 22:40:18 +0530

    solution

---
+++ src/components/Footer/index.js
@@ -0,0 +1,9 @@
+import './index.css';
+
+const Footer = () => (
+    <footer>
+        <h1>This is the Footer</h1>
+    </footer>
+);
+
+export default Footer;
---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import './index.css';
+
+const Header = () => (
+    <header>
+        <h1>This is the Header</h1>
+    </header>
+);
+
+export default Header;
